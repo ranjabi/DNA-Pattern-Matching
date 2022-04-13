a173eb4aa3b15a2a216dd7f2732e284530a1567f
ranjabi <fikriranjabi02@gmail.com>
2022-04-13 23:17:43 +0700

    add "add disease" sql functionality

--- src/client/src/components/AddDisease.js
+++ src/client/src/components/AddDisease.js
@@ -1,14 +1,71 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
 const AddDisease = () => {
-    return (
-        <div class="App">
-            <h1>Add Disease</h1>
-            <label>Nama Penyakit:</label>
-            <input type="text" name="disease"/>
-            <label>Sequence DNA:</label>
-            <button>Upload File</button>
-            <button type="submit">Submit</button>
-        </div>
-    )
-}
+  const [diseaseName, setDiseaseName] = useState("");
+  const [DNASequence, setDNASequence] = useState("");
+  const [diseasesList, setDiseasesList] = useState([]);
+
+  const diseaseNameChangeHandler = (event) => {
+    setDiseaseName(event.target.value);
+  };
+
+  const DNASequenceChangeHandler = (event) => {
+    setDNASequence(event.target.value);
+  };
+
+  const submitDisease = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      disease_name: diseaseName,
+      dna_sequence: DNASequence,
+    }).then(() => {
+      alert("Insert Success");
+    });
+    setDiseaseName("");
+    setDNASequence("");
+  };
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setDiseasesList(response.data);
+    });
+  });
+
+  const showDiseases = () => {};
+
+  return (
+    <div class="App">
+      <h1>Add Disease</h1>
+      <label>Nama Penyakit:</label>
+      <input
+        type="text"
+        name="disease-name"
+        onChange={diseaseNameChangeHandler}
+      />
+      <label>Sequence DNA:</label>
+      <input
+        type="text"
+        name="dna-sequence"
+        onChange={DNASequenceChangeHandler}
+      />
+      <button>Upload File</button>
+      <button type="submit" onClick={submitDisease}>
+        Submit
+      </button>
+
+      <p>Loaded sql:</p>
+      {diseasesList.map((val) => {
+        return (
+          <div>
+            <label>
+              ID: {val.id} Disease name: {val.disease_name} DNA Sequence:{" "}
+              {val.dna_sequence}
+            </label>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default AddDisease;
--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,44 @@
-const express = require("Express")
-const app = express()
-const mysql = require("mysql")
+const express = require("Express");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql");
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'dna',
-})
+  // configure user and password using your profile
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "dna",
+});
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/get", (req, res) => {
+  const sqlShowDisease = "SELECT * FROM diseases;";
+  db.query(sqlShowDisease, (err, result) => {
+    // console.log(result)
+    res.send(result);
+  });
+});
+
+app.post("/api/insert", (req, res) => {
+  const disease_name = req.body.disease_name;
+  const dna_sequence = req.body.dna_sequence;
+
+  const sqlInsertDisease =
+    "INSERT INTO diseases (disease_name, dna_sequence) VALUES (?, ?);";
+  db.query(sqlInsertDisease, [disease_name, dna_sequence], (err, result) => {
+    console.log(result);
+  });
+});
 
 app.get("/", (req, res) => {
-
-    const sqlInsert = "INSERT INTO diseases (disease_name, dna_sequence) VALUES ('flu', 'AGCTTGCAACGTACGTACTA');"
-    db.query(sqlInsert, (err, result) => {
-        res.send("query executed")
-    })
-})
+  res.send("Selamat datang di Dufan!");
+});
 
 app.listen(3001, () => {
-    console.log("Server is connected")
-})
+  console.log("Server is connected");
+});
