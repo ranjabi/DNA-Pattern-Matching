5f91d7e416d4d303d7b0029fe6109548043a60fe
Leoni Antoinette <leoniantoinette12@gmail.com>
2022-04-24 21:19:28 +0700

    update date format

--- src/server/regexSearch.js
+++ src/server/regexSearch.js
@@ -17,50 +17,33 @@ exports.regexSearchTerm = function(str) {
        2 : valid, input tanggal namaPenyakit
        3 : valid, input namaPenyakit */
     if (reTgl1.test(str) || reTgl2.test(str)) {
-        if (reTgl1.test(str)) {
-            var date = str.split(/-|\//);
-            date[0] = date[0].padStart(2, '0');
-            date[1] = date[1].padStart(2, '0');
-            // setDateSearch(date.reverse().join("-"));
-            dateSearch = date.reverse().join("-");
-        } else {
-            var date = str.split(/\s/);
-            date[0] = date[0].padStart(2, '0');
-            date[1] = convertMonth(date[1]);
-            // setDateSearch(date.reverse().join("-"));
-            dateSearch = date.reverse().join("-");
-        }
+        var date = str.split(/-|\/|\s/);
+        date[0] = date[0].padStart(2, '0');
+        date[1] = convertMonth(date[1]);
+        dateSearch = date.join(" ");
         console.log("date " + dateSearch);
-        // return 1;
         searchMethod = 1;
     } else if (reTglNama1.test(str) || reTglNama2.test(str)) {
         if (reTglNama1.test(str)) {
             var elmt = str.split(/\s/);
             var date = elmt[0].split(/-|\//);
             date[0] = date[0].padStart(2, '0');
-            date[1] = date[1].padStart(2, '0');
-            // setDateSearch(date.reverse().join("-"));
-            // setNameSearch(elmt.slice(1, elmt.length).join(" "));
-            dateSearch = date.reverse().join("-");
+            date[1] = convertMonth(date[1]);
+            dateSearch = date.join(" ");
             nameSearch = elmt.slice(1, elmt.length).join(" ");
         } else {
             var elmt = str.split(/\s/);
             elmt[0] = elmt[0].padStart(2, '0');
             elmt[1] = convertMonth(elmt[1]);
-            // setDateSearch(elmt.slice(0,3).reverse().join("-"));
-            // setNameSearch(elmt.slice(3, elmt.length).join(" "));
-            dateSearch = elmt.slice(0,3).reverse().join("-");
+            dateSearch = elmt.slice(0,3).join(" ");
             nameSearch = elmt.slice(3, elmt.length).join(" ");
         }
         console.log("date " + dateSearch);
         console.log("name " + nameSearch);
-        // return 2;
         searchMethod = 2;
     } else if (reNama.test(str)) {
-        // setNameSearch(str);
         nameSearch = str;
         console.log("name " + nameSearch);
-        // return 3;
         searchMethod = 3;
     }
     return {
@@ -71,29 +54,29 @@ exports.regexSearchTerm = function(str) {
 }
 
 const convertMonth = (month) => {
-    if (month.match(/(j|J)an(?:uary)?/)) {
-        return "01";
-    } else if (month.match(/(f|F)eb(?:ruary)?/)) {
-        return "02";
-    } else if (month.match(/(m|M)ar(?:ch)?/)) {
-        return "03";
-    } else if (month.match(/(a|A)pr(?:il)?/)) {
-        return "04";
-    } else if (month.match(/(May|may)/)) {    
-        return "05";
-    } else if (month.match(/(j|J)un(?:e)?/)) {
-        return "06";
-    } else if (month.match(/(j|J)ul(?:y)?/)) {
-        return "07";
-    } else if (month.match(/(a|A)ug(?:ust)?/)) {
-        return "08";
-    } else if (month.match(/(s|S)ep(?:t)?(?:tember)?/)) {
-        return "09";
-    } else if (month.match(/(o|O)ct(?:ober)?/)) {
-        return "10";
-    } else if (month.match(/(n|N)ov(?:ember)?/)) {
-        return "11";
-    } else if (month.match(/(d|D)ec(?:ember)?/)) {
-        return "12";
+    if (month.match(/(j|J)an(?:uary)?/) || month.match(/0?1/)) {
+        return "January";
+    } else if (month.match(/(f|F)eb(?:ruary)?/) || month.match(/0?2/)) {
+        return "February";
+    } else if (month.match(/(m|M)ar(?:ch)?/) || month.match(/0?3/)) {
+        return "March";
+    } else if (month.match(/(a|A)pr(?:il)?/) || month.match(/0?4/)) {
+        return "April";
+    } else if (month.match(/(May|may)/) || month.match(/0?5/)) {    
+        return "May";
+    } else if (month.match(/(j|J)un(?:e)?/) || month.match(/0?6/)) {
+        return "June";
+    } else if (month.match(/(j|J)ul(?:y)?/) || month.match(/0?7/)) {
+        return "July";
+    } else if (month.match(/(a|A)ug(?:ust)?/) || month.match(/0?8/)) {
+        return "August";
+    } else if (month.match(/(s|S)ep(?:t)?(?:tember)?/) || month.match(/0?9/)) {
+        return "September";
+    } else if (month.match(/(o|O)ct(?:ober)?/) || month.match(/10/)) {
+        return "October";
+    } else if (month.match(/(n|N)ov(?:ember)?/) || month.match(/11/)) {
+        return "November";
+    } else if (month.match(/(d|D)ec(?:ember)?/) || month.match(/12/)) {
+        return "December";
     }
 }
